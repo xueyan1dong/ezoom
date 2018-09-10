51bd2dcd3e97ceb55d5f5491ae51ff4dc07632ca
Xueyan Dong <susan.dong@ithelps.co>
2018-09-10 18:54:44 -0400

    change to order_detail table to add line_num

--- database/create_order_detail.sql
+++ database/create_order_detail.sql
@@ -4,17 +4,20 @@
 *    Created By             : Xueyan Dong
 *    Date Created           : 2009
 *    Platform Dependencies  : MySql
-*    Description            : 
+*    Description            : record products being ordered
 *    example	            : 
 *    Log                    :
-*    6/19/2018: Peiyu Ge: added header info. 					
+*    6/19/2018: Peiyu Ge: added header info. 
+*    09/10/2018: Xueyan Dong: added new column line_num
+				
 */
 DELIMITER $  -- for escaping purpose
 DROP TABLE IF EXISTS `order_detail`$
 CREATE TABLE `order_detail` (
-  `order_id` int(10) unsigned NOT NULL,
+  `order_id` int(11) unsigned NOT NULL,
   `source_type` enum('product', 'material') NOT NULL,
-  `source_id` int(10) unsigned NOT NULL,
+  `source_id` int(11) unsigned NOT NULL,
+  `line_num` smallint(5) unsigned NOT NULL DEFAULT 1,
   `quantity_requested` decimal(16,4) unsigned NOT NULL,
   `unit_price` decimal(10,2) unsigned DEFAULT NULL,
   `quantity_made` decimal(16,4) unsigned NOT NULL DEFAULT '0',
@@ -24,8 +27,8 @@ CREATE TABLE `order_detail` (
   `output_date` datetime DEFAULT NULL,
   `expected_deliver_date` datetime DEFAULT NULL,  
   `actual_deliver_date` datetime DEFAULT NULL,
-  `recorder_id` int(10) unsigned NOT NULL,
+  `recorder_id` int(11) unsigned NOT NULL,
   `record_time` datetime NOT NULL,
   `comment` text,
-  PRIMARY KEY (`order_id`, `source_type`, `source_id`)
+  PRIMARY KEY (`order_id`, `source_type`, `source_id`, `line_num`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8$
