0ad0743f7ed1a7bdb129d8be240bebb9b76d91ee
Peiyu <yulier.ge@gmail.com>
2019-01-01 11:29:26 -0500

    implemented loadlocation web page

---
+++ ezMESWeb/Configure/Location/LoadLocation.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/Configure/ConfigureModule.master" AutoEventWireup="true" CodeBehind="LoadLocation.aspx.cs" Inherits="ezMESWeb.Configure.Location.LoadLocation" Title= "Load Location -- ezOOM" %>
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="asp" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+<asp:ToolKitScriptManager ID ="ToolKitScriptManager1" runat ="server"></asp:ToolKitScriptManager>
+
+<br />
+<p><em>This tool only read csv (comma separated values) file. CSV headers are required. Please save your file as .csv file before upload. </em></p><br />
+<p>Required columns: name, parent_loc_id, adjacent_loc_id1, adjacent_loc_id2, adjacent_loc_id3, adjacent_loc_id4, contact_employee, description, comment.</p>
+<br />
+<asp:FileUpload ID="fuLoad" runat="server" Width=500px Height=30px />
+<br /><br />
+<asp:Button ID="btnInsert" runat="server" Text="Preview" Width="147px" OnClick="btn_Click"/>     
+<br />
+<asp:Label ID="lblError" runat="server" ForeColor="#FF3300" Height="60px" Width="350px"></asp:Label><br />
+<p><em>ezOOM is going to update following information in ezOOM, please review and click Submit button at bottom to start updating.</em></p>
+<br />
+<asp:TextBox ID="txtContent" runat="server" TextMode="MultiLine" Height=1100px Width=800px OnTextChanged="txtContent_TextChanged"></asp:TextBox>
+<br />
+<asp:Label ID="lblErrorSubmit" runat="server" ForeColor="#FF3300" Height="60px" Width="350px"><br />
+<asp:Button ID="btnSubmit" runat="server" Text="Submit" Width="147px" OnClick="btnSubmit_Click"/> 
+</asp:Label><asp:Label ID ="lblLoaded" runat="server"></asp:Label>
+<asp:Panel ID="MessagePanel" runat="server" CssClass="detail" style="margin-top: 19px;  height:200px; width:400px; display:none" HorizontalAlign="Center" BorderColor="Green">
+<asp:Button id="btnMessagePopup" runat="server" style="display:none" />
+<asp:ModalPopupExtender ID="MessagePopupExtender" runat="server" TargetControlID="btnMessagePopup" BackgroundCssClass="modalBackground" PopupControlID="MessagePanel" PopupDragHandleControlID="MessagePanel" Drag="True" DropShadow="True" ></asp:ModalPopupExtender>    
+<table width=90% height=90% cellpadding=5px cellspacing = 10px align="center" >
+<tr><td>&nbsp;</td></tr>
+<tr><td>
+<asp:Label ID="lblMessage" runat="server"  Style="font-family:Times New Roman; font-style:italic; font-size:20px; line-height:22px; color:Green;">
+Congratulations! The data have been successfully loaded into database. Please click below button to  </asp:Label>
+</td></tr>
+<tr><td>
+<asp:Button ID="btnLocForm" runat="server" Text="Go To Location" Style="font-family:Times New Roman;font-size:16px; font-weight:bold; width:200px;" onclick="btnLocForm_Click" />
+</td></tr>
+</table>           
+</asp:Panel>
+</asp:Content>
---
+++ ezMESWeb/Configure/Location/LoadLocation.aspx.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using LumenWorks.Framework.IO.Csv;
+using System.IO;
+using System.Data;
+
+namespace ezMESWeb.Configure.Location
+{
+    public partial class LoadLocation : ConfigTemplate//System.Web.UI.Page
+    {
+
+        string[] cols = { "name", "parent_loc_id", "contact_employee", "adjacent_loc_id1", "adjacent_loc_id2", "adjacent_loc_id3", "adjacent_loc_id4", "description", "comment" };
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_Click(object sender, EventArgs e)
+        {
+            DataRow newRow;
+            string field = "";
+            string itemContent = "";
+            string loaded = "";
+            lblError.Text = "";
+            using (CsvReader csv = new CsvReader(new StreamReader(fuLoad.PostedFile.InputStream), true))
+            {
+                string[] headers = csv.GetFieldHeaders();
+                DataTable csvTable = new DataTable();
+                for(int i=0; i<headers.Length; i++)
+                {
+                    csvTable.Columns.Add(headers[i]);
+                }
+                while (csv.ReadNextRecord())
+                {
+                    newRow = csvTable.NewRow();
+                    for(int i=0; i<headers.Length; i++)
+                    {
+                        newRow[i] = csv[i];
+                    }
+                    csvTable.Rows.Add(newRow);
+                }
+                //preview
+                for(int j=0; j < csvTable.Rows.Count; j++)
+                {
+                    itemContent += "Location " + (j+1) + ":\n";
+                    for(int k=0; k<cols.Length; k++)
+                    {
+                        try
+                        {
+                            field = csvTable.Rows[j][cols[k].ToString()].ToString();
+                        }catch(Exception ex)
+                        {
+                            lblError.Text = ex.Message;
+                        }
+                        itemContent += cols[k].ToString() + ": " + field + "\n";
+                        loaded += cols[k].ToString() + ":" + field + ";";
+                    }
+                 
+                    itemContent += "\n";
+                    loaded += "|";
+                    
+                }
+                txtContent.Text = itemContent;
+                lblLoaded.Text = loaded;
+            }
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string response;
+            try
+            {
+                lblErrorSubmit.Text = "";
+                ConnectToDb();
+                ezCmd = new CommonLib.Data.EzSqlClient.EzSqlCommand();
+                ezCmd.Connection = ezConn;
+                ezCmd.CommandText = "modify_location";
+                ezCmd.CommandType = CommandType.StoredProcedure;
+                
+                //name:x;parent_loc_id:x;...|name:x; ;
+                string[] locations = lblLoaded.Text.Split(new string[] { "|" }, StringSplitOptions.RemoveEmptyEntries);
+                int n = locations.Length;
+                for (int i = 0; i < locations.Length; i++)
+                {
+                    ezCmd.Parameters.AddWithValue("@_location_id", DBNull.Value, ParameterDirection.InputOutput);
+                    string[] fields = locations[i].Split(new string[] { ";" }, StringSplitOptions.RemoveEmptyEntries);
+                    for (int j = 0; j < fields.Length; j++)
+                    {
+                        string[] field = fields[j].Split(new string[] { ":" }, StringSplitOptions.RemoveEmptyEntries);
+                        string key = "@_"+ field[0];
+                        string val = field[1];
+                        if (val.Length == 0)
+                        {
+                            ezCmd.Parameters.AddWithValue(key, DBNull.Value, ParameterDirection.Input);
+                            //ezCmd.Parameters[key.ToString()].Value = DBNull.Value;
+                        }
+                        else
+                        {
+                            ezCmd.Parameters.AddWithValue(key, val, ParameterDirection.Input);
+                            //ezCmd.Parameters[key.ToString()].Value = val;
+                        }
+                    }
+                    ezCmd.Parameters.AddWithValue("@_response", DBNull.Value, ParameterDirection.Output);
+                    ezCmd.ExecuteNonQuery();
+                    response = ezCmd.Parameters["@_response"].Value.ToString();
+                    if (response.Length > 0)
+                    {
+                        lblErrorSubmit.Text = response;
+                        break;
+                    }
+                    ezCmd.Parameters.Clear();
+                }
+
+            }
+            catch(Exception ex)
+            {
+                lblErrorSubmit.Text = ex.Message;
+            }
+            ezCmd.Dispose();
+            ezConn.Dispose();
+            if (lblErrorSubmit.Text.Length == 0)
+            {
+                MessagePopupExtender.Show();
+            }
+        }
+
+
+        protected void txtContent_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnLocForm_Click(object sender, EventArgs e)
+        {
+            MessagePopupExtender.Hide();
+            Server.Transfer("LocationConfigure.aspx");
+        }
+    }
+}
