e6df83b0a3173493ccfa0caf105e0c95a750ec67
Peiyu <yulier.ge@gmail.com>
2019-04-02 09:11:11 -0400

    added LoadProduct

---
+++ ezMESWeb/Configure/Product/LoadProduct.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/Configure/ConfigureModule.master" AutoEventWireup="true" CodeBehind="LoadProduct.aspx.cs" Inherits="ezMESWeb.Configure.Product.LoadProduct" Title= "Load Product -- ezOOM" %>
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="asp" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+<asp:ToolkitScriptManager ID="ToolkitScriptManager1" runat="server" />
+
+<br />
+<p><em>This tool only read csv (comma separated values) file. Please save your file as .csv file before upload.</em></p>
+<br />
+    <asp:FileUpload ID="fuLoad1" runat="server" Width=500px Height=30px />
+<br /><br />
+<asp:Button ID="btnInsert" runat="server" Text="Preview" Width="147px" OnClick="btn_Click"/>     
+<br />
+<asp:Label ID="lblError" runat="server" ForeColor="#FF3300" 
+                Height="60px" Width="350px"></asp:Label><br />
+    <p>Required columns: Product_Group, Name, Lot_Size, Unit, LifeSpan, Description, Comment.</p><br />
+    <p>Examples: general, TestProduct, 10, unit, 5, a test description, a test comment</p><br />
+    <p><em>ezOOM is going to update following information in ezOOM, please review and click Submit button at bottom to start updating.</em></p>
+ <br />   <asp:TextBox ID="txtContent1" runat="server" TextMode="MultiLine" Height=1100px Width=800px OnTextChanged="txtContent_TextChanged"></asp:TextBox>
+<br />
+ <asp:ListBox ID="lbVendor" runat="server" Visible="false"></asp:ListBox>
+<br />
+ <asp:ListBox ID="lbMaterial1" runat="server" Visible="false"></asp:ListBox>
+<br />
+<asp:Button ID="btnSubmit" runat="server" Text="Submit" Width="147px" OnClick="btnSubmit_Click"/>  
+   <asp:Panel ID="MessagePanel" runat="server" CssClass="detail" 
+   style="margin-top: 19px;  height:200px; width:400px; display:none" HorizontalAlign="Center" BorderColor="Green">
+ <asp:Button id="btnMessagePopup" runat="server" style="display:none" />
+ <asp:ModalPopupExtender ID="MessagePopupExtender1" runat="server" TargetControlID="btnMessagePopup"
+     BackgroundCssClass="modalBackground" PopupControlID="MessagePanel" 
+    PopupDragHandleControlID="MessagePanel" Drag="True" DropShadow="True" >
+    </asp:ModalPopupExtender>    
+    <table width=90% height=90% cellpadding=5px cellspacing = 10px align="center" >
+    <tr><td>&nbsp;</td></tr>
+    <tr><td>
+    <asp:Label ID="lblMessage" runat="server"  Style="font-family:Times New Roman; font-style:italic; font-size:20px; line-height:22px; color:Green;">
+    Congratulations! The data have been successfully loaded into database. Please click below button to  </asp:Label>
+    </td></tr>
+
+    <tr><td>
+    <asp:Button ID="btnInvForm" runat="server" Text="Go To Inventory Form" 
+                Style="font-family:Times New Roman;font-size:16px; font-weight:bold; width:200px;" 
+                onclick="btnInvForm_Click" />
+     </td></tr>
+     </table>           
+   </asp:Panel>
+</asp:Content>
---
+++ ezMESWeb/Configure/Product/LoadProduct.aspx.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.Configuration;
+using AjaxControlToolkit;
+using LumenWorks.Framework.IO.Csv;
+using System.IO;
+using System.Globalization;
+
+namespace ezMESWeb.Configure.Product
+{
+    public partial class LoadProduct : ConfigTemplate
+    {
+
+        protected FileUpload fuLoad1;
+        protected TextBox txtContent1;
+        protected ListBox lbMaterial1;
+        protected ModalPopupExtender MessagePopupExtender1;
+
+        protected void btn_Click(object sender, EventArgs e)
+        {
+            load_csv();
+        }
+
+
+        protected void load_csv()
+        {
+
+            DataRow newRow;
+            string field;
+            string itemContent = "";
+            string count = "";
+            txtContent1.Text = "";
+            lblError.Text = "";
+
+            Int32 fileLen = fuLoad1.PostedFile.ContentLength;
+
+            using (CsvReader csv = new CsvReader(new StreamReader(fuLoad1.PostedFile.InputStream), true))
+            {
+                string[] headers = csv.GetFieldHeaders();
+
+                DataTable csvTable = new DataTable();
+                for (int i = 0; i < headers.Length; i++)
+                    csvTable.Columns.Add(headers[i]);
+
+
+                while (csv.ReadNextRecord())
+                {
+                    newRow = csvTable.NewRow();
+                    for (int i = 0; i < headers.Length; i++)
+                    {
+                        newRow[i] = csv[i];//default is trimmed.
+
+                    }
+                    csvTable.Rows.Add(newRow);
+                }
+
+                txtContent1.Text += "Insert following inventory information:\n\n";
+                itemContent += "Load following item information:\n\n";
+
+                //lbVendor.Items.Clear();
+                lbMaterial1.Items.Clear();
+                Int32 materialIndex = 0;
+
+                try
+                {
+                    //connect to DB
+                    ConnectToDb();
+                    ezCmd = new CommonLib.Data.EzSqlClient.EzSqlCommand();
+                    ezCmd.Connection = ezConn;
+
+                    for (int j = 0; j < csvTable.Rows.Count; j++)
+                    {
+                        //check source_type is empty
+                        field = csvTable.Rows[j]["Product_Group"].ToString();
+                        if (field.Length != 0)
+                        {
+                            ezCmd.CommandText = "Select id from product_group where name = '" + field + "'";
+                            ezCmd.CommandType = CommandType.Text;
+                            try
+                            {
+                                count = ezCmd.ExecuteScalar().ToString();
+
+                                //now can store Source Type in lbMaterial
+                                lbMaterial1.Items.Add(count);
+                                itemContent += "Product_Group:" + field + "\n";
+
+                                //Product name
+                                field = csvTable.Rows[j]["Name"].ToString();
+
+                                if (field.Length != 0)
+                                {
+                                    lbMaterial1.Items[materialIndex].Text += "||" + field;
+                                    itemContent += "Name:" + field + "\n";
+                                    field = csvTable.Rows[j]["Lot_Size"].ToString();
+                                    lbMaterial1.Items[materialIndex].Text += "||" + field;
+                                    itemContent += "Lot_Size:" + field + "\n";
+
+                                    field = csvTable.Rows[j]["Unit"].ToString(); //unit is not null
+                                    ezCmd.CommandText = "Select id from uom where name = '" + field + "'";
+                                    if (field.Length != 0)
+                                    {
+                                        try
+                                        {
+                                            count = ezCmd.ExecuteScalar().ToString();
+                                            lbMaterial1.Items[materialIndex].Text += "||" + count;
+                                            itemContent += "Unit:" + field + "\n";
+                                        }
+                                        catch (Exception ex)
+                                        {
+                                            lblError.Text = String.Concat("Unit in row ", j + 1, " not found Please verify and upload again.");
+                                            break;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        lblError.Text = "Unit is necessary.";
+                                        break;
+                                    }
+
+                                    field = csvTable.Rows[j]["LifeSpan"].ToString();
+                                    if(field.Length != 0)
+                                    {
+                                        lbMaterial1.Items[materialIndex].Text += "||" + field;
+                                        itemContent += "LifeSpan:" + field + "\n";
+                                    }
+                                    else{
+                                        lblError.Text = "LifeSpan is necessary.";
+                                        break;
+                                    }
+                                    
+                                    field = csvTable.Rows[j]["Description"].ToString();
+                                    lbMaterial1.Items[materialIndex].Text += "||" + field;      
+                                    itemContent += "Description:" + field + "\n";
+
+                                    field = csvTable.Rows[j]["Comment"].ToString();
+                                    lbMaterial1.Items[materialIndex].Text += "||" + field;
+                                    itemContent += "Commmment:" + field + "\n";
+
+                                    itemContent += "\n";
+                                    
+                                    materialIndex++;
+    
+                                }
+                                else
+                                {
+                                    lblError.Text = String.Concat("Name in row ", j + 1, " is necessary");
+                                    break;
+                                }
+
+                            }
+                            catch(Exception ex)
+                            {
+                                lblError.Text = String.Concat("Product_Group in row ", j + 1, " not found. Please verify and upload again.");
+                                break;
+                            }   
+                        }
+                        else
+                        {
+                            lblError.Text = String.Concat("Product_Group in row ", j + 1, " is necessary.");
+                            break;
+                        }
+                    }
+                    txtContent1.Text += "\n" + itemContent;
+                    ezCmd.Dispose();
+                }
+                catch (Exception ex)
+                {
+                    lblError.Text = ex.Message;
+                }
+
+            }
+
+        }
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string response;
+            string[] fields;
+            if (Page.IsValid)
+            {
+                if (lbMaterial1.Items.Count == 0 | lblError.Text.Length != 0)
+                {
+                    load_csv();
+                }
+                if (lblError.Text.Length == 0)
+                {
+
+                    try
+                    {
+
+                        ConnectToDb();
+                        ezCmd = new CommonLib.Data.EzSqlClient.EzSqlCommand();
+                        ezCmd.Connection = ezConn;
+
+                        string today = DateTime.UtcNow.Year + "-" + DateTime.UtcNow.Month + "-" + DateTime.UtcNow.Day;
+                        string datePattern = "MM/dd/yyyy";
+                        DateTime parsedDate;
+
+                        ezCmd.CommandText = "modify_product";
+                        ezCmd.Parameters.Clear();
+
+                        
+                        ezCmd.Parameters.AddWithValue("@_product_id", DBNull.Value, ParameterDirection.InputOutput);
+                        ezCmd.Parameters.AddWithValue("@_created_by", Convert.ToInt32(Session["UserID"]));
+                        ezCmd.Parameters.AddWithValue("@_version", 1);
+                        ezCmd.Parameters.AddWithValue("@_state", "production");
+                        ezCmd.Parameters.AddWithValue("@_pg_id", DBNull.Value);
+                        ezCmd.Parameters.AddWithValue("@_name", DBNull.Value);
+                        ezCmd.Parameters.AddWithValue("@_lot_size", DBNull.Value);
+                        ezCmd.Parameters.AddWithValue("@_uomid", DBNull.Value);
+                        ezCmd.Parameters.AddWithValue("@_lifespan", DBNull.Value);//today);
+                        ezCmd.Parameters.AddWithValue("@_description", DBNull.Value);
+                        ezCmd.Parameters.AddWithValue("@_comment", DBNull.Value);
+                        ezCmd.Parameters.AddWithValue("@_response", DBNull.Value, ParameterDirection.Output);
+
+                        string[] spliter = { "||" };
+                        for (int i = 0; i < lbMaterial1.Items.Count; i++)
+                        {
+                            //Product_Group, Name, Lot_Size, Unit, LifeSpan, Description, Comment.</p><br />
+
+                            fields = lbMaterial1.Items[i].Text.Split(spliter, StringSplitOptions.None);
+                            ezCmd.Parameters["@_product_id"].Value = DBNull.Value;
+                            ezCmd.Parameters["@_pg_id"].Value = fields[0];
+                            ezCmd.Parameters["@_name"].Value = fields[1];
+                            if(fields[2].Length != 0)
+                            {
+                                ezCmd.Parameters["@_lot_size"].Value = fields[2];
+                            }
+                           
+                            ezCmd.Parameters["@_uomid"].Value = fields[3];
+                            ezCmd.Parameters["@_lifespan"].Value = fields[4];
+                            if (fields[5].Length != 0)
+                            {
+                                ezCmd.Parameters["@_description"].Value = fields[5];
+                            }
+                            if (fields[6].Length != 0)
+                            {
+                                ezCmd.Parameters["@_comment"].Value = fields[6];
+                            }
+                          
+                            ezCmd.ExecuteNonQuery();
+                            response = ezCmd.Parameters["@_response"].Value.ToString();
+                            if (response.Length > 0)
+                            {
+                                lblError.Text = response;
+                                break;
+                            }
+                            else
+                            {
+                                lbMaterial1.Items[i].Value = ezCmd.Parameters["@_product_id"].Value.ToString();
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        lblError.Text = ex.Message;
+                    }
+                    ezCmd.Dispose();
+                    ezConn.Dispose();
+                    if (lblError.Text.Length == 0)
+                    {
+                        MessagePopupExtender1.Show();
+                    }
+
+                }
+            }
+        }
+
+        protected void txtContent_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnInvForm_Click(object sender, EventArgs e)
+        {
+            MessagePopupExtender1.Hide();
+            Server.Transfer("ProductConfig.aspx");
+        }
+    }
+
+}
