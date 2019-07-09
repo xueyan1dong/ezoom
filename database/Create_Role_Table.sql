81582adea716c0f46fd64b7e1cfa9e71cfb44668
Xueyan Dong <susan.dong@ithelps.co>
2019-07-08 20:56:40 -0400

    add description column to system_role table

--- database/Create_Role_Table.sql
+++ database/Create_Role_Table.sql
@@ -7,29 +7,32 @@
 *    Description            : 
 *    example	            : 
 *    Log                    :
-*    6/19/2018: Peiyu Ge: added header info. 					
+*    6/19/2018: Peiyu Ge: added header info. 	
+*    7/8/2019: Xueyan Dong: added column: description	to hold the description of privileges of the role		
 */
-DELIMITER $  -- for escaping purpose
+DELIMITER $ 
 DROP TABLE IF EXISTS `system_roles`$
 CREATE TABLE `system_roles` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `applicationId` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,  -- internal db id of the role
+  `applicationId` int(11) NOT NULL,  -- the id of the ezoom version. for now, it defaults to 1
+  `name` varchar(255) NOT NULL,  -- name of the role
+  `description` TEXT NULL,  -- describe the privileges that the role enjoy here
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC$
 
-Insert into `system_roles` ( `applicationId`, `name`) values (1, 'Admin');
-Insert into `system_roles` ( `applicationId`, `name`) values (1, 'Manager');
-Insert into `system_roles` ( `applicationId`, `name`) values (1, 'QA');
-Insert into `system_roles` ( `applicationId`, `name`) values (1, 'Engineer');
+Insert into `system_roles` ( `applicationId`, `name`, `description`) values (1, 'Admin', 'Administrator of eZOOM. Has access to all forms')$
+Insert into `system_roles` ( `applicationId`, `name`, `description`) values (1, 'Manager', 'Manager on Shop floor. Has access to all forms')$
+Insert into `system_roles` ( `applicationId`, `name`, `description`) values (1, 'QA', 'Quality Assurance. Has access to all forms')$
+Insert into `system_roles` ( `applicationId`, `name`, `description`) values (1, 'Engineer', 'Product Engineer. Has access to all forms')$
+Insert into `system_roles` ( `applicationId`, `name`, `description`) values (1, 'Operator', 'Operator on Shop floor. Has access to Move Product by Operator form')$
 
-DROP TABLE IF EXISTS `users_in_roles`;
+DROP TABLE IF EXISTS `users_in_roles`$
 CREATE TABLE `users_in_roles` (
   `userId` int(11) NOT NULL DEFAULT '0',
   `roleId` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`userId`,`roleId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC$
 
-Insert into `users_in_roles` (`userId`, `roleId`) values(1, 1);
-Insert into `users_in_roles` (`userId`, `roleId`) values(2, 1);
+Insert into `users_in_roles` (`userId`, `roleId`) values(1, 1)$
+Insert into `users_in_roles` (`userId`, `roleId`) values(2, 1)$
 Insert into `users_in_roles` (`userId`, `roleId`) values(3, 3)$
--- database/load_tables.sql
+++ database/load_tables.sql
@@ -1053,15 +1053,33 @@ CREATE TABLE `feedback` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8$
 
 -- system_roles table
+/*
+*    Copyright 2009 ~ Current  IT Helps LLC
+*    Source File            : Create_Role_Table.sql
+*    Created By             : Xueyan Dong
+*    Date Created           : 2009
+*    Platform Dependencies  : MySql
+*    Description            : 
+*    example	            : 
+*    Log                    :
+*    6/19/2018: Peiyu Ge: added header info. 	
+*    7/8/2019: Xueyan Dong: added column: description	to hold the description of privileges of the role		
+*/
+DELIMITER $ 
 DROP TABLE IF EXISTS `system_roles`$
 CREATE TABLE `system_roles` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `applicationId` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,  -- internal db id of the role
+  `applicationId` int(11) NOT NULL,  -- the id of the ezoom version. for now, it defaults to 1
+  `name` varchar(255) NOT NULL,  -- name of the role
+  `description` TEXT NULL,  -- describe the privileges that the role enjoy here
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC$
 
--- users_in_roles table
+Insert into `system_roles` ( `applicationId`, `name`, `description`) values (1, 'Admin', 'Administrator of eZOOM. Has access to all forms')$
+Insert into `system_roles` ( `applicationId`, `name`, `description`) values (1, 'Manager', 'Manager on Shop floor. Has access to all forms')$
+Insert into `system_roles` ( `applicationId`, `name`, `description`) values (1, 'QA', 'Quality Assurance. Has access to all forms')$
+Insert into `system_roles` ( `applicationId`, `name`, `description`) values (1, 'Engineer', 'Product Engineer. Has access to all forms')$
+Insert into `system_roles` ( `applicationId`, `name`, `description`) values (1, 'Operator', 'Operator on Shop floor. Has access to Move Product by Operator form')$
 
 DROP TABLE IF EXISTS `users_in_roles`$
 CREATE TABLE `users_in_roles` (
@@ -1069,6 +1087,10 @@ CREATE TABLE `users_in_roles` (
   `roleId` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`userId`,`roleId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC$
+
+Insert into `users_in_roles` (`userId`, `roleId`) values(1, 1)$
+Insert into `users_in_roles` (`userId`, `roleId`) values(2, 1)$
+Insert into `users_in_roles` (`userId`, `roleId`) values(3, 3)$
 
 -- view_ingredient view
 DROP VIEW IF EXISTS `view_ingredient`$
